2efafb2795d0af6e4669b19c9c2dce0bdc83276a
Mihailo Boskovic <mihailob5017@gmail.com>
2019-12-11 22:18:20 +0100

    its built

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Mern-Notes
+To start the app you will need to have a backend runing.To start both the backend and the frontend.Simply "npm run dev" from the root 
---
+++ frontend/build/precache-manifest.e4cc93d46daab542c1da6ed68aa5c83d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19430c30cb4c14613923de0e08863152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9f24ca795dc4da74dd",
+    "url": "/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "90aeafba9679d0341b24",
+    "url": "/static/css/main.58083abc.chunk.css"
+  },
+  {
+    "revision": "5c9f24ca795dc4da74dd",
+    "url": "/static/js/2.7c2ce538.chunk.js"
+  },
+  {
+    "revision": "18b6a5bf156c218c69888d92513e7b9e",
+    "url": "/static/js/2.7c2ce538.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90aeafba9679d0341b24",
+    "url": "/static/js/main.e12e6621.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  }
+]);
